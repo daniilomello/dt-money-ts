40f1b2fc70400a6327b0bab54815359cf08f9252
Danilo Mello <danilodemellow@gmail.com>
2022-03-29 21:22:22 -0300

    feat: add transactions table components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { GlobalStyle } from "./styles/global.style";
 import Header from "./components/header/header.component";
 import Dashboard from "./components/dashboard/dashboard.component";
+import TransactionsTable from "./components/transactions/transactions.component";
 
 export default function App() {
   return (
--- src/components/dashboard/dashboard.component.tsx
+++ src/components/dashboard/dashboard.component.tsx
@@ -1,10 +1,12 @@
 import Summary from "../summary/summary.component";
+import TransactionsTable from '../transactions/transactions.component';
 import { Container } from "./dashboard.styles";
 
 const Dashboard = () => {
   return (
     <Container>
       <Summary />
+      <TransactionsTable />
     </Container>
   );
 };
