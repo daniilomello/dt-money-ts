dad22bc72a8144d7236e567c771cf7f99848d10c
Danilo Mello <danilodemellow@gmail.com>
2022-03-30 23:28:48 -0300

    feat: add form modal styles

--- src/components/new-transaction-modal/new-transaction-modal.component.tsx
+++ src/components/new-transaction-modal/new-transaction-modal.component.tsx
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import { api } from '../../services/axios/api';
+import { useState, FormEvent } from 'react';
+import {Container, TransactionTyperContainer, RadioBox} from './new-transaction-modal.styles';
 import Modal from 'react-modal';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import closeModal from '../../assets/close.svg';
-import {Container, TransactionTyperContainer, RadioBox} from './new-transaction-modal.styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -11,29 +12,80 @@ interface NewTransactionModalProps {
 }
 
 const NewTransactionModal = ({isOpen, onRequestClose}:NewTransactionModalProps) => {
+  const [title, setTitle] = useState('');
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit');
 
+  function handleCreateNewTransaction(event: FormEvent){
+    event.preventDefault();
+
+    const data = {
+      title,
+      value,
+      category,
+      type
+    };
+
+    api.post('/transactions', data)
+  }
+
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestClose} overlayClassName="react-modal-overlay" className="react-modal-content">
-      <button type="button" onClick={onRequestClose} className="react-modal-close">
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content">
+      <button
+        type="button"
+        onClick={onRequestClose}
+        className="react-modal-close">
         <img src={closeModal} alt="Fechar modal icone" />
       </button>
-      <Container>
+
+      <Container onSubmit={handleCreateNewTransaction}>
         <h2>Cadastrar transação</h2>
 
-        <input type="text" placeholder="Título" />
-        <input type="number" placeholder="Valor" />
+        <input
+          type="text"
+          placeholder="Título"
+          onChange={event => setTitle(event.target.value)}
+        />
+
+        <input
+          type="number"
+          placeholder="Valor"
+          onChange={event => setValue(+event.target.value)}
+        />
+
         <TransactionTyperContainer>
-          <RadioBox type="button" onClick={() => setType('deposit')} isActive={type === 'deposit'} activeColor="green">
+          <RadioBox
+            type="button"
+            onClick={() => setType('deposit')}
+            isActive={type === 'deposit'}
+            activeColor="green"
+          >
             <img src={incomeImg} alt="Entrada" />
             <span>Entrada</span>
           </RadioBox>
-          <RadioBox type="button" onClick={() => setType('withdraw')} isActive={type === 'withdraw'} activeColor="red">
+
+          <RadioBox
+            type="button"
+            onClick={() => setType('withdraw')}
+            isActive={type === 'withdraw'}
+            activeColor="red"
+          >
             <img src={outcomeImg} alt="Entrada" />
             <span>Saída</span>
           </RadioBox>
         </TransactionTyperContainer>
-        <input type="text" placeholder="Categoria" />
+
+        <input
+          type="text"
+          placeholder="Categoria"
+          onChange={event => setCategory(event.target.value)}
+        />
+
         <button type="submit">Cadastrar</button>
       </Container>
     </Modal>
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,45 @@
 import { render } from "react-dom";
-import {createServer} from 'miragejs';
+import {createServer, Model} from 'miragejs';
 import App from "./App";
 
 createServer({
-  routes() {
-    this.namespace = 'api';
-    this.get('/transactions', () => {
-      return [
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server){
+    server.db.loadData({
+      transactions: [
         {
           id: 1,
-          title: 'Desenvolvimento de Website',
-          amount: 12000,
+          title: 'Website WordPress',
           type: 'deposit',
-          category: 'Desenvolvimento',
-          createdAt: new Date()
+          category: 'Trampo',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1000,
+          createdAt: new Date('2021-02-14 14:00:00')
         }
       ]
+    })
+  },
+
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return this.schema.all('transaction');
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+      return schema.create('transaction', data);
     })
   }
 })
